a7c57c6ff6b79d7af51d558a6ca8c99a9c32c348
Bolude Daniel <boludedaniel@gmail.com>
2025-12-24 23:19:47 +0100

    added referrer provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ReferrerProvider>
+          
 {children}
         </ReferrerProvider>
       </body>
