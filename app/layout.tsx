adfe96b3d773ca6a9646c46df1038e2c0d722adf
Bolude Daniel <boludedaniel@gmail.com>
2025-12-24 23:15:35 +0100

    added provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import ReferrerProvider from "./ReffererProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ReferrerProvider>
+{children}
+        </ReferrerProvider>
       </body>
     </html>
   );
